86453d0d6525f7da12520d75153ef58757f2e51e
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-09-03 16:47:41 +0300

    Initial commit for linked list task

---
+++ CustomLinkedList/src/main/java/LinkedList.java
@@ -0,0 +1,38 @@
+public class LinkedList<T> {
+    public int size() {
+        return 0;
+    }
+
+    public void addFirst(T el) {
+    }
+
+    public void addLast(T el) {
+    }
+
+    public void add(int index, T el) {
+    }
+
+    public T getFirst() {
+        return null;
+    }
+
+    public T getLast() {
+        return null;
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    public T remove(int index) {
+        return null;
+    }
+}
---
+++ CustomLinkedList/src/test/java/LinkedListTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTest {
+    @Test
+    void sizeTest() {}
+
+    @Test
+    void addFirstTest() {}
+
+    @Test
+    void addLastTest() {}
+
+    @Test
+    void addTest() {}
+
+    @Test
+    void getFirstTest() {}
+
+    @Test
+    void getLastTest() {}
+
+    @Test
+    void getTest() {}
+
+    @Test
+    void removeFirstTest() {}
+
+    @Test
+    void removeLastTest() {}
+
+    @Test
+    void removeTest() {}
+}
