203d3bb823b9f64f8882343d1f3e7053395bfa75
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-09-10 21:10:39 +0300

    package name changed

--- CustomLinkedList/src/main/java/model/LinkedList.java
+++ CustomLinkedList/src/main/java/model/LinkedList.java
@@ -1,4 +1,4 @@
-package list;
+package model;
 
 import java.util.NoSuchElementException;
 
--- CustomLinkedList/src/test/java/LinkedListTest.java
+++ CustomLinkedList/src/test/java/LinkedListTest.java
@@ -1,4 +1,4 @@
-import list.LinkedList;
+import model.LinkedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
